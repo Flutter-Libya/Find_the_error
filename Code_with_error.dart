e2c949b32dd8643ad50f664fa8beafde8ba85c92
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-11-23 00:18:09 +0200

    Create Code_with_error.dart
    this code has error can you know what is it?

---
+++ Code_with_error.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Counter: $counter'),
+            RaisedButton(
+              onPressed: _incrementCounter,
+              child: Text('Increment'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+}
